ea8702f6e3135a96b068457f95722aa312fca838
Sett17 <raik.rohde@gmail.com>
2024-04-18 13:51:59 +0200

    feat(cli): implement status command functionality
    Enhanced the status command in the CLI to check the connection to the backend using team name and password. It now also logs information like config file location, making it more informative and user-friendly.

--- cmd/cli/cmd/status.go
+++ cmd/cli/cmd/status.go
@@ -1,8 +1,8 @@
 package cmd
 
 import (
-	"fmt"
-
+	"github.com/snippetaccumulator/snac/internal/backend/request"
+	"github.com/snippetaccumulator/snac/internal/log"
 	"github.com/spf13/cobra"
 )
 
@@ -13,7 +13,20 @@ var statusCmd = &cobra.Command{
 	Long: `Checks the status to the backend, using team name and password.
 Will also show other relevant information like config file location`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("status called")
+		log.Info("Checking status...")
+		reqBuilder := request.NewRequestBuilder()
+		req := reqBuilder.
+			ForTeamByID(config.TeamName, config.Password, false).
+			Check().Build()
+		_ = req
+		retData, retType, err := req.Execute(db)
+		log.Err(true, err)
+		err = request.TypeCheck(retData, retType)
+		log.Err(true, err)
+
+		log.Success("Connection check for team '%s' successful", config.TeamName)
+
+		log.Info("Config file location: %s", configLoc)
 	},
 }
 
